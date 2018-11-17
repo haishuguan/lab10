80c277978a78bc6f1397d3c2e4ac2a180a4706aa
haishuguan <43195107+haishuguan@users.noreply.github.com>
2018-11-16 21:49:26 -0500

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -1,6 +1,6 @@
-var num1 = document.getElementById("number1").value;
-var num2 = document.getElementById("numebr2").value;
-var total = num1 + num2;
 function output(){
-  document.getElementById("output").innerHTML = total;
+  var num1 = document.getElementById("number1").value;
+  var num2 = document.getElementById("numebr2").value;
+  var sum = num1 + num2;
+  document.getElementById("output").innerHTML = sum;
 }
